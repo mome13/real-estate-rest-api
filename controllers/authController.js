3839bcea3a2444114104916fd7db636f7476ccef
mehran13mome <mehran13mome@gmail.com>
2023-03-27 08:50:05 +0330

    Feat: Add register controller

---
+++ controllers/authController.js
@@ -0,0 +1,32 @@
+const authController = require('express').Router();
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+authController.post('/register', async (req, res) => {
+  try {
+    const isExisting = await User.findOne({ email: req.body.email });
+
+    if (isExisting) {
+      return res
+        .status(500)
+        .json({ msg: 'Email is already taken by another user.' });
+    }
+
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    const newUser = await User.create({
+      ...req.body,
+      password: hashedPassword,
+    });
+
+    const { password, ...others } = newUser._doc;
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+      expiresIn: '4h',
+    });
+
+    return res.status(201).json({ others, token });
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+});
