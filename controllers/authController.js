1d04f8052628c2f8c6aa5f21fb0ef96f83625a0d
mehran13mome <mehran13mome@gmail.com>
2023-03-27 08:50:20 +0330

    Feat: add login controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -30,3 +30,27 @@ authController.post('/register', async (req, res) => {
     return res.status(500).json(error.message);
   }
 });
+
+authController.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(500).json({ msg: 'Wrong credentials. Try again!' });
+    }
+
+    const comparePass = await bcrypt.compare(req.body.password, user.password);
+    if (!comparePass) {
+      return res.status(500).json({ msg: 'Wrong credentials. Try again!' });
+    }
+
+    const { password, ...others } = user._doc;
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: '4h',
+    });
+
+    console.log({ others, token });
+    return res.status(200).json({ others, token });
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+});
