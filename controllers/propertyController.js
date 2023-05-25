2d70fe7dc098fd08ddfe4780ac1bab08052c7cd9
mehran13mome <mehran13mome@gmail.com>
2023-05-25 14:15:23 +0330

    feat/api: add property find with filter

--- controllers/propertyController.js
+++ controllers/propertyController.js
@@ -1,65 +1,69 @@
 const verifyToken = require('../middlewares/verifyToken');
 const Property = require('../models/Property');
 const propertyController = require('express').Router();
+const {
+  searchAllPropertiesValidator,
+} = require('../validators/propertyValidator');
+const { validate } = require('express-validation');
+const PropertyQueryBuilder = require('../utils/propertyBuilder');
 
-// get all
-propertyController.get('/getAll', async (req, res) => {
-  try {
-    const properties = await Property.find({});
-    return res.status(200).json(properties);
-  } catch (error) {
-    console.error(error);
-  }
-});
+// search with filters (add builder design pattern)
+propertyController.get(
+  '/find',
+  validate(searchAllPropertiesValidator),
+  async (req, res) => {
+    const {
+      type,
+      area,
+      bedrooms,
+      bathrooms,
+      parkings,
+      address,
+      minRent,
+      maxRent,
+      minPrice,
+      maxPrice,
+    } = req.query;
+    let properties = [];
+    try {
+      const propertyBuilder = new PropertyQueryBuilder.Builder()
+        .setType(type)
+        .setBathrooms(bathrooms)
+        .setBedrooms(bedrooms)
+        .setParkings(parkings)
+        .setAddress(address)
+        .setMinRent(minRent)
+        .setMaxRent(maxRent)
+        .setMinPrice(minPrice)
+        .setMaxPrice(maxPrice)
+        .build();
 
-// get featured
-propertyController.get('/find/featured', async (req, res) => {
-  try {
-    const featuredProperties = await Property.find({ featured: true }).populate(
-      'currentOwner',
-      '-password'
-    );
-    return res.status(200).json(featuredProperties);
-  } catch (error) {
-    return res.status(500).json(error);
-  }
-});
 
-// get all from type
-propertyController.get('/find', async (req, res) => {
-  const type = req.query;
-  let properties = [];
-  try {
-    if (type) {
-      properties = await Property.find(type).populate('owner', '-password');
-    } else {
-      properties = await Property.find({});
+        console.log('property', propertyBuilder , propertyBuilder.this);
+      // properties = await Property.find({
+      //   price: { $gt: minPrice, $lt: maxPrice },
+      //   rent: { $gt: minRent, $lt: maxRent },
+      //   address: { $regex: addresses, $options: 'i' },
+      //   type: { $eq: type },
+      //   parkings: { $gt: parkings },
+      //   bedrooms: { $gt: bedrooms },
+      //   bathrooms: { $gt: bathrooms },
+      // });
+      
+      properties = await Property.find(propertyBuilder);
+
+      return res.status(200).json(properties);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json(error);
     }
-
-    return res.status(200).json(properties);
-  } catch (error) {
-    return res.status(500).json(error);
   }
-});
-
-propertyController.get('/find/types', async (req, res) => {
-  try {
-    const houseType = await Property.countDocuments({ type: 'house' });
-    const apartmentType = await Property.countDocuments({ type: 'apartment' });
-
-    return res.status(200).json({ house: houseType, apartment: apartmentType });
-  } catch (error) {
-    return res.status(500).json(error);
-  }
-});
+);
 
 // TODO FETCH INDIVIDUAL PROPERTY
 propertyController.get('/find/:id', async (req, res) => {
   try {
-    const property = await Property.findById(req.params.id).populate(
-      'currentOwner',
-      '-password'
-    );
+    const property = await Property.findById(req.params.id);
 
     if (!property) {
       throw new Error('No such property with that id');
@@ -121,4 +125,28 @@ propertyController.put('/:id', verifyToken, async (req, res) => {
   }
 });
 
-module.exports = propertyController
+// // get featured
+// propertyController.get('/find/featured', async (req, res) => {
+//   try {
+//     const featuredProperties = await Property.find({ featured: true }).populate(
+//       'currentOwner',
+//       '-password'
+//     );
+//     return res.status(200).json(featuredProperties);
+//   } catch (error) {
+//     return res.status(500).json(error);
+//   }
+// });
+
+// propertyController.get('/find/types', async (req, res) => {
+//   try {
+//     const houseType = await Property.countDocuments({ type: 'house' });
+//     const apartmentType = await Property.countDocuments({ type: 'apartment' });
+
+//     return res.status(200).json({ house: houseType, apartment: apartmentType });
+//   } catch (error) {
+//     return res.status(500).json(error);
+//   }
+// });
+
+module.exports = propertyController;
