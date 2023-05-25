a4289a3694a336d3d6b5f5680974f421c7e02b36
mehran13mome <mehran13mome@gmail.com>
2023-05-25 14:16:54 +0330

    feat: add authentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,8 +2,13 @@ const authController = require('express').Router();
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const {
+  signupValidator,
+  signinValidator,
+} = require('../validators/authValidator');
+const { validate } = require('express-validation');
 
-authController.post('/register', async (req, res) => {
+authController.post('/signup', validate(signupValidator), async (req, res) => {
   try {
     const isExisting = await User.findOne({ email: req.body.email });
 
@@ -16,22 +21,22 @@ authController.post('/register', async (req, res) => {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
     const newUser = await User.create({
-      ...req.body,
+      email: req.body.email,
       password: hashedPassword,
     });
 
-    const { password, ...others } = newUser._doc;
+    const { _id, email } = newUser._doc;
     const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
       expiresIn: '4h',
     });
 
-    return res.status(201).json({ others, token });
+    return res.status(201).json({ _id, email, token });
   } catch (error) {
     return res.status(500).json(error.message);
   }
 });
 
-authController.post('/login', async (req, res) => {
+authController.post('/signin', validate(signinValidator), async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email });
     if (!user) {
@@ -43,14 +48,15 @@ authController.post('/login', async (req, res) => {
       return res.status(500).json({ msg: 'Wrong credentials. Try again!' });
     }
 
-    const { password, ...others } = user._doc;
+    const { _id, email } = user._doc;
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
       expiresIn: '4h',
     });
 
-    console.log({ others, token });
-    return res.status(200).json({ others, token });
+    return res.status(200).json({ _id, email, token });
   } catch (error) {
     return res.status(500).json(error.message);
   }
 });
+
+module.exports = authController;
